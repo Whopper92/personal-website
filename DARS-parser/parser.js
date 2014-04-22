87660f6d751e5ac4d7057bf704618c6892519a52
Will Hopper <w.hopper922@gmail.pdx.edu>
2014-04-22 16:30:08 +0000

    Update DARS parser to dynamically find requirements

--- DARS-parser/parser.js
+++ DARS-parser/parser.js
@@ -1,9 +1,7 @@
-function getCSData() {
+function parseReport() {
   var report = document.getElementById("reportInput").value;
 
-  // Create regular expressions to parse the text
   // Overall credits
-  var regCredOverall     = /(NO|OK|IP).*Student must complete a minimum of \d*/;
   var regCredTotalEarned = /EARNED:? {0,2}?(\d{1,3}\.\d{2}) CREDITS/;
   var regCredInProg      = /IN-PROGRESS:? {0,2}?(\d{1,3}\.\d{2}) CREDITS/;
   var regCredNeeded      = /NEEDS:? *(\d{1,3}\.\d{2}) CREDITS/;
@@ -12,67 +10,23 @@ function getCSData() {
   var regUpCredEarned = /\( *(\d{1,3}\.\d{2}) HOURS TAKEN *\)/; // the first one
   var regUpCredInProg = /In-Prog-> *(\d{1,3}.\d{2}) CREDITS/; // The first one
 
-  // PSU Residence Credit Requirements / Pass No Pass Limits
-  var regResStatus  = /(NO|OK|IP) *(PSU RESIDENCE CREDIT REQUIREMENTS)/;
-  var regLastCreds  = /(.) *1\) (45 of last 60 credits must be taken at PSU)/;
-  var regPassNoPass = /(.) *2\) (25 of the last 45 credits must be for grade)/;
+  var mainRequirements = []
+  var subRequirements  = []
+  mainRequirements     = report.match(/(NO  |OK  |IP  )(?!=)(.*)/g);
+  subRequirements      = report.match(/(\+|\-) *\d{1,2}\)(.*)/g);
 
-  // University writing requirements
-  var regWrStatus = /(NO|OK|IP) *(Writing Requirement)/;
-  var regLowerWr  = /(.) *1\) (One lower division Writing course)/;
-  var regAddWr    = /(.) *2\) (Additional Writing Course)/;
-
-  // Bachelor of Science Requirements
-  var regBsStatus = /(NO|OK|IP) *(Bachelor of Science Requirements)/;
-
-  // UNST Requirements
-  var regFrInq = /(.)  7\) Complete (COMM 220 or Freshman Inquiry)/;
-  var regSopInq   = /(NO|OK|IP) *UNIVERSITY STUDIES - (Sophomore Inquiry)/;
-  var regJunClus  = /(NO|OK|IP) *UNIVERSITY STUDIES - (Upper Division Cluster)/;
-
-  // Computer Science major requirements
-  var regCsStatus    = /(NO|OK|IP) *(COMPUTER SCIENCE MAJOR REQUIREMENTS)/;
-  var regCsSciElec   = /(.) *1\) (Science electives) complete --/;
-  var regCsWr227     = /(.) *3\) (Complete WR 227)--/;
-  var regCsReqMth    = /(.) *5\) Complete the following (MTH\/STAT courses) --/;
-  var regCsMthElec   = /(.) *6\) Choose 7 credits of (approved MTH electives) from the/;
-  var regCsReqLower  = /(.) *8\) Complete the following (required lower division)/;
-  var regCsReqUpper  = /(.) *9\) Complete the following (required upper division)/;
-  var regCs305       = /(.) *10\) Complete (CS 305) Social, Ethical, and Legal/;
-  var regCsUpperElec = /(.) *11\) Complete 12 credits of (upper division CS electives)/;
-  var regCsECE341    = /(.) *13\) Complete (ECE 341) --/;
-
-  // Performing matches
-  var credOverall     = regCredOverall.exec(report);
+  // Credit and GPA matches
   var credTotalEarned = regCredTotalEarned.exec(report);
   var credInProg      = regCredInProg.exec(report);
+  if(credInProg == null) { credInProg = ['0.00', '0.00'] }
   var upCredEarned    = regUpCredEarned.exec(report);
   var upCredInProg    = regUpCredInProg.exec(report);
-  var resStatus       = regResStatus.exec(report);
-  var lastCreds       = regLastCreds.exec(report);
-  var passNoPass      = regPassNoPass.exec(report);
-  var wrStatus        = regWrStatus.exec(report);
-  var lowerWr         = regLowerWr.exec(report);
-  var addWr           = regAddWr.exec(report);
-  var bsStatus        = regBsStatus.exec(report);
-  var frInq           = regFrInq.exec(report);
-  var sopInq          = regSopInq.exec(report);
-  var junClus         = regJunClus.exec(report);
-  var csStatus        = regCsStatus.exec(report);
-  var csSciElec       = regCsSciElec.exec(report);
-  var csWr227         = regCsWr227.exec(report);
-  var csReqMth        = regCsReqMth.exec(report);
-  var csMthElec       = regCsMthElec.exec(report);
-  var csReqLower      = regCsReqLower.exec(report);
-  var csReqUpper      = regCsReqUpper.exec(report);
-  var cs305           = regCs305.exec(report);
-  var csUpperElec     = regCsUpperElec.exec(report);
-  var csECE341        = regCsECE341.exec(report);
-
+  if(upCredInProg == null) { upCredInProg = ['0.00', '0.00'] }
 
   // Needed credits section - we have to do this because of DARS formatting
   var neededCreditArray = [];
   neededCreditArray = report.match(/NEEDS:? *(\d{1,3}\.\d{2}) CREDITS/g);
+  if(neededCreditArray == null) { neededCreditArray = ['0.00', '0.00'] }
   var credNeeded    = neededCreditArray[0].match(/\d{1,3}\.\d{2}/);
   var upCredNeeded  = neededCreditArray[1].match(/\d{1,3}\.\d{2}/);
 
@@ -93,32 +47,28 @@ function getCSData() {
   document.getElementById('cumuGpaCell').innerHTML= '<b>' + cumuGPA + '<b>';
   document.getElementById('majorGpaCell').innerHTML= '<b>' + majGPA + '<b>';
 
-  // Visualize the general education column
-  var genEdCol = document.getElementById('genEdColumn');
-  var genEdArray = [resStatus, lastCreds, passNoPass, wrStatus, lowerWr, addWr, bsStatus, frInq, sopInq, junClus]
+  // Visualize the main requirements column
+  var genReqCol = document.getElementById('genReqColumn');
+  var subReqCol = document.getElementById('subReqColumn');
 
-  genEdArray.forEach(function(entry){
-    if(entry[1] != 'OK' && entry[1] != '+') {
-      var rectClasses = 'dataRect incomplete';
-    } else {
+  mainRequirements.forEach(function(entry){
+    if(entry.substring(0,2) == 'OK') {
       var rectClasses = 'dataRect complete';
+    } else if(entry.substring(0,2) == 'IP') {
+      var rectClasses = 'dataRect inProgress';
+    } else {
+      var rectClasses = 'dataRect incomplete';
     }
-
-    genEdCol.insertAdjacentHTML('beforeend', '<div class="' + rectClasses + '">' + entry[2] + '</div>');
+    genReqCol.insertAdjacentHTML('beforeend', '<div class="' + rectClasses + '">' + entry.substring(2) + '</div>');
   });
 
-  // Visualize the major requirements column
-  var majorCol = document.getElementById('majorColumn');
-  var majorReqArray = [csStatus, csSciElec, csWr227,csReqMth, csMthElec, csReqLower, csReqUpper, cs305, csUpperElec, csECE341];
-
-  majorReqArray.forEach(function(entry){
-    if(entry[1] != 'OK' && entry[1] != '+') {
-      var rectClasses = 'dataRect incomplete';
-    } else {
-      var rectClasses = 'dataRect complete';
-    }
-
-    majorCol.insertAdjacentHTML('beforeend', '<div class="' + rectClasses + '">' + entry[2] + '</div>');
-  });
-
+// Visualize the sub requirements column
+   subRequirements.forEach(function(entry){
+     if(entry.substring(0,1) == '+') {
+       var rectClasses = 'dataRect complete';
+     } else {
+       var rectClasses = 'dataRect incomplete';
+     }
+     subReqCol.insertAdjacentHTML('beforeend', '<div class="' + rectClasses + '">' + entry.substring(5) + '</div>');
+   });
 }
